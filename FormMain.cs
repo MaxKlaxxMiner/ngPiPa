9a1e5bc5eac234ccd88afaee8408e10bc19a776c
MaxKlaxxMiner <max@tetrisk.de>
2019-04-08 17:51:55 +0200

    pixel test

--- FormMain.cs
+++ FormMain.cs
@@ -1,29 +1,38 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace ngPiPa
 {
   public sealed partial class FormMain : Form
   {
+    int[] pix = null;
+
     public FormMain()
     {
       InitializeComponent();
+      var rnd = new Random();
+      pix = new int[2048 * 2048];
+      for (int i = 0; i < pix.Length; i++) pix[i] = (rnd.Next() & 0xffffff) - 16777216;
     }
 
-    private void buttonColor_Select_Click(object sender, System.EventArgs e)
+    private void buttonColor_Select_Click(object sender, EventArgs e)
     {
       pictureBoxSelectedColor.BackColor = ((Button)sender).BackColor;
       splitToolSelector.Panel2.Refresh();
     }
 
+    int cellSize = 16;
+
     private void splitToolSelector_Panel2_Paint(object sender, PaintEventArgs e)
     {
       var panel = (Panel)sender;
-      var g = panel.CreateGraphics();
-      var r = panel.ClientRectangle;
-      var c = pictureBoxSelectedColor.BackColor;
-      g.FillRectangle(new SolidBrush(c), r);
-      g.DrawLine(new Pen(Color.FromArgb(0x0080ff - 16777216)), 10, 10, r.Width - 20, r.Height - 20);
+      var g = e.Graphics;
+      int startX = 0;
+      int startY = 0;
+      int endX = panel.Width;
+      int endY = panel.Height;
+
     }
   }
 }
